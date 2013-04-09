1255e750fd2b4771ae8034c86d851656760d5d9c
jejip <jip@Jips-MacBook-Pro-2.local>
2013-04-09 10:07:56 +0200

    De juiste met tabs en goeie sturen enzo

--- fakir/fakir.ino
+++ fakir/fakir.ino
@@ -129,7 +129,53 @@ void loop(){
   dInput = (anglespeed * (3.14159/180));
   myPID.Compute();
   
-    //send-receive with processing if it's time
+  
+    // Bereken stuur waarden en pas ze toe op de output.
+  steerValue = analogRead(steerPin);
+     if(steerValue < 300){
+       left  = Output + 10;
+       right = Output;
+//       steerOffset_left = 100; //(int)(300 - steerValue)/100;
+//       steerOffset_right =-100; // -steerOffset_left;
+     }
+     else if(steerValue > 723){
+        left = Output;
+        right = Output + 10;       
+//       steerOffset_right = 100; //(int)(steerValue - 723)/100;
+//       steerOffset_left = -100; //-steerOffset_right;
+     }
+     else {
+       left = Output;
+       right = Output;
+//       steerOffset_left = 0;
+//       steerOffset_right = 0;
+     }
+  
+       //LEDjes voor richting aangeven
+     if (left > Output)
+     {
+       digitalWrite(10, HIGH);
+     }
+     else
+     {
+       digitalWrite(10, LOW);
+     }
+     if (right > Output)
+     {
+       digitalWrite(10, HIGH);
+     }
+     else
+     {
+       digitalWrite(10, LOW);
+     }
+     //stuur data naar de motor
+  b_left = (byte)left;
+  b_right = (byte)right;
+  
+//  b_left = (byte)Output; // + (byte)steerOffset_left;
+//  b_right = (byte)Output; // + (byte)steerOffset_right;
+  
+      //send-receive with processing if it's time
   if(millis()>serialTime)
   {
     SerialReceive();
@@ -137,7 +183,7 @@ void loop(){
     serialTime+=20;
   }
   
-    //killswitch, voordat hij waardes naar de motor stuurt
+      //killswitch, voordat hij waardes naar de motor stuurt
 buttonState = digitalRead(8);
 
 if(buttonState == LOW){ //als de switch naar zwart staat, ga uit
@@ -148,165 +194,11 @@ if(buttonState == LOW){ //als de switch naar zwart staat, ga uit
     myPID.SetMode(AUTOMATIC); //zet anders de PID aan
   }
   
-  
-    // Bereken stuur waarden en pas ze toe op de output.
-  steerValue = analogRead(steerPin);
-  if(angle >= .5 && angle <= -.5 ){
-     if(steerValue < 300){
-       left  = Output + 20;
-       right = Output - 20;
-//       steerOffset_left = 100; //(int)(300 - steerValue)/100;
-//       steerOffset_right =-100; // -steerOffset_left;
-     }
-     else if(steerValue > 723){
-        left = Output - 20;
-        right = Output + 20;       
-//       steerOffset_right = 100; //(int)(steerValue - 723)/100;
-//       steerOffset_left = -100; //-steerOffset_right;
-     }
-     else {
-       left = Output;
-       right = Output;
-//       steerOffset_left = 0;
-//       steerOffset_right = 0;
-     }
-  }
-   
-  
-  //stuur data naar de motor
-  b_left = (byte)left;
-  b_right = (byte)right;
-  
-//  b_left = (byte)Output; // + (byte)steerOffset_left;
-//  b_right = (byte)Output; // + (byte)steerOffset_right;
-  
   //stuur data naar de motor
   sendData(CMDBYTE, motorDir, SPEEDBYTE, b_left, b_right); 
 
 
 } //end loop
-
-// *** PID serial communication functions ***
-
-union {                // This Data structure lets
-  byte asBytes[24];    // us take the byte array
-  float asFloat[6];    // sent from processing and
-}                      // easily convert it to a
-foo;                   // float array
-
-
-// getting float values from processing into the arduino
-// was no small task.  the way this program does it is
-// as follows:
-//  * a float takes up 4 bytes.  in processing, convert
-//    the array of floats we want to send, into an array
-//    of bytes.
-//  * send the bytes to the arduino
-//  * use a data structure known as a union to convert
-//    the array of bytes back into an array of floats
-
-//  the bytes coming from the arduino follow the following
-//  format:
-//  0: 0=Manual, 1=Auto, else = ? error ?
-//  1: 0=Direct, 1=Reverse, else = ? error ?
-//  2-5: float setpoint
-//  6-9: float input
-//  10-13: float output  
-//  14-17: float P_Param
-//  18-21: float I_Param
-//  22-245: float D_Param
-
-void SerialReceive()
-{
-
-  // read the bytes sent from Processing
-  int index=0;
-  byte Auto_Man = -1;
-  byte Direct_Reverse = -1;
-  while(Serial.available()&&index<26)
-  {
-    if(index==0) Auto_Man = Serial.read();
-    else if(index==1) Direct_Reverse = Serial.read();
-    else foo.asBytes[index-2] = Serial.read();
-    index++;
-  } 
-  
-  // if the information we got was in the correct format, 
-  // read it into the system
-  if(index==26  && (Auto_Man==0 || Auto_Man==1)&& (Direct_Reverse==0 || Direct_Reverse==1))
-  {
-    Setpoint=double(foo.asFloat[0]);
-    //Input=double(foo.asFloat[1]);       // * the user has the ability to send the 
-                                          //   value of "Input"  in most cases (as 
-                                          //   in this one) this is not needed.
-    if(Auto_Man==0)                       // * only change the output if we are in 
-    {                                     //   manual mode.  otherwise we'll get an
-      Output=double(foo.asFloat[2]);      //   output blip, then the controller will 
-    }                                     //   overwrite.
-    
-    double p, i, d;                       // * read in and set the controller tunings
-    p = double(foo.asFloat[3]);           //
-    i = double(foo.asFloat[4]);           //
-    d = double(foo.asFloat[5]);           //
-    myPID.SetTunings(p, i, d);            //
-    
-    if(Auto_Man==0) myPID.SetMode(MANUAL);// * set the controller mode
-    else myPID.SetMode(AUTOMATIC);             //
-    
-    if(Direct_Reverse==0) myPID.SetControllerDirection(DIRECT);// * set the controller Direction
-    else myPID.SetControllerDirection(REVERSE);          //
-  }
-  Serial.flush();                         // * clear any random data from the serial buffer
-}
-
-// unlike our tiny microprocessor, the processing ap
-// has no problem converting strings into floats, so
-// we can just send strings.  much easier than getting
-// floats from processing to here no?
-void SerialSend()
-{
-  Serial.print("PID ");
-  Serial.print(Setpoint);   
-  Serial.print(" ");
-  Serial.print(Input);   
-  Serial.print(" ");
-  Serial.print(Output);   
-  Serial.print(" ");
-  Serial.print(myPID.GetKp());   
-  Serial.print(" ");
-  Serial.print(myPID.GetKi());   
-  Serial.print(" ");
-  Serial.print(myPID.GetKd());   
-  Serial.print(" ");
-  if(myPID.GetMode()==AUTOMATIC) Serial.print("Automatic");
-  else Serial.print("Manual");  
-  Serial.print(" ");
-  if(myPID.GetDirection()==DIRECT) Serial.println("Direct");
-  else Serial.println("Reverse");
-}
-
-
-// *** motor functions ***
-
-void sendData(byte dirReg, byte dirVal, byte speedReg, byte speedVal_left, byte speedVal_right){         // Function for sending data to MD03
-  Wire.beginTransmission(ADDRESS1);         // Send data to MD03
-    Wire.write(dirReg);
-    Wire.write(dirVal);
-    Wire.write(speedReg);
-    Wire.write(speedVal_left);
-  Wire.endTransmission();
-//  Serial.print("Wire return: ");
-//  Serial.println(b);
-  
-  Wire.beginTransmission(ADDRESS2);
-    Wire.write(dirReg);
-    Wire.write(dirVal);
-    Wire.write(speedReg);
-    Wire.write(speedVal_right);
-  Wire.endTransmission();
-//  Serial.println(b);  
-}  
-
 
 
   
